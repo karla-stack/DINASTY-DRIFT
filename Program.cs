3d7f2f16ee5cf972a0a644ac2dd53b316c7a6bf9
karla-stack <karlabencosme14@gmail.com>
2024-06-30 15:13:47 -0400

    Co-authored-by: ManuWok12 <ManuWok12@users.noreply.github.com> Co-authored-by: Héctor <Swzyy@users.noreply.github.com>

--- Program.cs
+++ Program.cs
@@ -1,13 +1,16 @@
 ﻿using System;
 namespace Proyecto
 {
-    class Progam
+    class Program
     {
         static void Main()
         {
             Console.WriteLine("Inserte un numero: ");
             string? numberAsString = Console.ReadLine();
             int number = Convert.ToInt32(numberAsString);
+
+            
+            
         }
     }
 }
