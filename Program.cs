1441169d4a440550884995dd8d50031609547603
karla-stack <karlabencosme14@gmail.com>
2024-06-29 21:30:04 -0400

    Co-authored-by: Héctor <Swzyy@users.noreply.github.com>

--- Program.cs
+++ Program.cs
@@ -7,6 +7,7 @@ namespace Proyecto
         {
             Console.WriteLine("Cambios");
             Console.WriteLine("Pruebas");
+            Console.WriteLine("Prueba de nuevo");
         }
     }
 }
