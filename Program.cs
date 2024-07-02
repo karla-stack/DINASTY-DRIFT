ac26c4047b0d7e1b23f6be5547c3136853f942e1
karla-stack <karlabencosme14@gmail.com>
2024-07-02 13:20:18 -0400

    Co-authored-by: Héctor <Swzyy@users.noreply.github.com> Co-authored-by: Isaac Romero Rojas <20221437@itla.edu.do>
    Co-authored-by: Enmanuel L. Perez <lenmanuel254@gmail.com>

--- Program.cs
+++ Program.cs
@@ -5,11 +5,60 @@ namespace Proyecto
     {
         static void Main()
         {
-            Console.WriteLine("Inserte un numero: ");
-            string? numberAsString = Console.ReadLine();
-            int number = Convert.ToInt32(numberAsString);
+            bool game = false;
 
-            
+            while(!game)
+            {
+
+                Console.WindowHeight = 40; //UI/UX 
+                Console.WindowHeight = 50;
+
+                Console.BackgroundColor = ConsoleColor.Black;
+                Console.ForegroundColor = ConsoleColor.DarkBlue;
+
+
+                
+                Console.WriteLine("Menú principal");  //Opciones del menú
+                Console.WriteLine("1. Iniciar juego");
+                Console.WriteLine("2. Instrucciones");
+                Console.WriteLine("3. Configuraciones");
+                Console.WriteLine("4. Acerca de ");
+                Console.WriteLine("5. Salir del juego");
+
+                Console.WriteLine("Seleccione una opcion ");
+                string? opcionAsString = Console.ReadLine(); // Pidiendo datos 
+
+                int opcion;
+                if(int.TryParse(opcionAsString, out opcion))//Convirtiendo datos
+                {
+                  switch(opcion)  // en los cases van los métodos 
+                  {
+                    case 1:
+                    break;
+
+                    case 2:
+                    break;
+
+                    case 3:
+                    break;
+
+                    case 4:
+                    break;
+
+                    case 5:
+                    game = true;
+                    Console.WriteLine("Gracias por jugar"); // El bucle se rompe y termina el programa
+                    break; 
+
+                   }
+                }
+                else
+                {
+                    Console.WriteLine("Error insertar un numero de menú válido"); //En caso de que el usuario ingrese un valor inválido
+                }
+
+
+            }
             
         }
     }
--- obj/Debug/net8.0/PRUEBA.AssemblyInfo.cs
+++ obj/Debug/net8.0/PRUEBA.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PRUEBA")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f8187150585b38f12a68c04ef6bf24acfc05912a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3d7f2f16ee5cf972a0a644ac2dd53b316c7a6bf9")]
 [assembly: System.Reflection.AssemblyProductAttribute("PRUEBA")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PRUEBA")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
